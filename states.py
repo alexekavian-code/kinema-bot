b96d0ee97d41c0ed71449fb71f7be450ce274e71
alexekavian-code <alex.ekavian@gmail.com>
2026-06-10 00:22:56 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,8 +1,13 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class SearchStates(StatesGroup):
     choosing_mode = State()
     choosing_genre = State()
     entering_actor = State()
     choosing_studio = State()
+    choosing_country = State()
+    choosing_decade = State()
+    entering_search = State()
     showing_results = State()
+    showing_favorites = State()
